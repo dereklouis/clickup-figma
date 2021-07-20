2b8051213f9dc20006b54ae411d61efdb2aa1520
Derek Louis <thedereklouis@gmail.com>
2021-07-20 11:14:21 -0400

    added webpack to bundle css files

---
+++ src/styles/index.js
@@ -0,0 +1,7 @@
+require('./features.css');
+require('./footer.css');
+require('./navBar.css');
+require('./section1.css');
+require('./section2.css');
+require('./style.css');
+require('./testimonials.css');
